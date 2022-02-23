13731a4a3f21e7b7f97d36a7a2eb104db2980b51
0244198 <100299598+0244198@users.noreply.github.com>
2022-02-23 09:34:49 -0600

    Create tarea1.py

---
+++ OOP/Herencia/tarea1.py
@@ -0,0 +1,59 @@
+class Trabajador:
+    def __init__(self, antiguedad, nombre):
+        self.antiguedad = antiguedad
+        self.nombre = nombre
+        self.salario = None
+        self.salario_base = None
+
+    def calcular_salario(self):
+        self.salario = self.salario_base + (self.antiguedad * 1000)
+        return self.salario
+
+    def trabaja(self):
+        raise NotImplementedError
+
+
+class Secretario(Trabajador):
+    def __init__(self, antiguedad, nombre):
+        super().__init__(antiguedad, nombre)
+        self.salario_base = 15000
+
+    def trabaja(self):
+        print("Apoya")
+
+
+class Programador(Trabajador):
+    def __init__(self, antiguedad, nombre):
+        super().__init__(antiguedad, nombre)
+        self.salario_base = 25000
+
+    def trabaja(self):
+        print("Programa")
+
+
+class Gerente(Trabajador):
+    def __init__(self, antiguedad, nombre):
+        super().__init__(antiguedad, nombre)
+        self.salario_base = 50000
+
+    def calcular_salario(self):
+        return 50000
+
+    def modificar_salario(self, objeto, aumento):
+        objeto.salario_base += aumento
+
+    def trabaja(self):
+        print("Gerencia")
+
+
+marcos = Programador(1, "Marcos")
+laura = Secretario(2, "Laura")
+adolfo = Gerente(3, "Adolfo")
+marcos.trabaja()
+laura.trabaja()
+adolfo.trabaja()
+print(marcos.calcular_salario())
+print(laura.calcular_salario())
+print(adolfo.calcular_salario())
+adolfo.modificar_salario(laura, 1)
+print(laura.calcular_salario())
